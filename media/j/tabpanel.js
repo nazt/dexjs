8958c2410ebd1cdd286147a9eadd7d691177f0c6
Nat Weerawan <nat.wrw@gmail.com>
2010-11-09 12:19:36 +0700

    End of my own

--- media/j/tabpanel.js
+++ media/j/tabpanel.js
@@ -1,16 +1,20 @@
 Ext.onReady(function() {
+var form_button = [{ xtype: 'button', text: 'Button'}, { xtype: 'button', text: 'Button'}]
+var form_element = [
+		{ xtype: 'label', text: 'Send to:', x: 0, y: 0 },
+		{ xtype: 'textfield', text: 'to', width: 200, x: 80, y: 0 },
+		{ xtype: 'label', text: 'Message:', x: 0, y: 30 },
+		{ xtype: 'textarea', text: 'msg', width: 200, x: 80, y: 30 },
+		{ xtype: 'container', autoEl: 'div', layout: 'form', columnWidth: 0.5, style: { padding: '10px'}, items: form_button},
+	]
 /*
 	new Ext.Viewport({
-		layout: 'absolute',
-		items: 
-		[
-			{ xtype: 'button', text: 'Button', x: 100, y: 100 },
-			{ xtype: 'label', text: 'Send to:', x: 0, y: 0 },
-			{ xtype: 'textfield', text: 'to', width: 200, x: 80, y: 0 },
-			{ xtype: 'label', text: 'Message:', x: 0, y: 30 },
-			{ xtype: 'textarea', text: 'msg', width: 200, x: 80, y: 30 },
-			{ xtype: 'tabpanel', text: 'msg', width: 200, x: 80, y: 30 }
-		]
+		layout: 'vbox',
+		layoutConfig: {
+			padding: '5',
+			align: 'left'
+		},
+		items: form_element 
 	});
 */
 	new Ext.Viewport({
@@ -23,7 +27,7 @@ Ext.onReady(function() {
                 activeTab: 0,
 				items: 
 				[
-					{ title: 'Tab 1', html: 'A simple tab' },
+					{ layoutConfig: { padding: '20', align: 'left'}, layout: 'vbox', title: 'Tab 1', items: form_element},
 					{ title: 'Tab 2', html: 'Another one' }
 				]
 			},
