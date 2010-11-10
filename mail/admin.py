8a1b94d3b9c6aa56d9e76594db5ccedf9027a785
Nat Weerawan <nat.wrw@gmail.com>
2010-11-10 12:12:36 +0700

    Change display field in mail admin

--- mail/admin.py
+++ mail/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from dexjs.mail.models import Mail
 
 class MailAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('subject', 'sender', 'message', 'created')
 
 admin.site.register(Mail, MailAdmin);
