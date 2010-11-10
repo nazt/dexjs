d1b284ea3a5d6ba08b979ba58418ea830eafc28a
Nat Weerawan <nat.wrw@gmail.com>
2010-11-10 12:13:43 +0700

    Save to database and render json response

--- mail/views.py
+++ mail/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.shortcuts import render_to_response, redirect
+from django.http import HttpResponse
+from django.core import serializers
+import json
+from dexjs.mail.models import Mail
+
+def sendmail(request):
+    _subject = request.POST.get('subject')
+    _sender = request.POST.get('sender')
+    _msg = request.POST.get('msg')
+    
+    m = Mail(subject=_subject, sender=_sender, message=_msg)
+    m.save()
+    print request.POST
+
+    ret = {'success': True, 'msg': 'Yes'}
+    return HttpResponse(json.dumps(ret));
+
